f56a19e389009d1a78a12239c893e5778afbf68a
limelnoll <limelnoll@gmail.com>
2022-08-26 15:41:30 +0200

    Update some info
    I update some crucial info, also I followed the conventions by replacing
    the double quotes by single quotes.

--- js/master.js
+++ js/master.js
@@ -1,314 +1,176 @@
-let mainColors = localStorage.getItem("color_option");
-
+let mainColors = localStorage.getItem('color_option');
 if (mainColors !== null) {
-
   document.documentElement.style.setProperty('--main-color', mainColors);
-
-    document.querySelectorAll(".colors-list li").forEach(element => {
-
-      element.classList.remove("active");
-
+    document.querySelectorAll('.colors-list li').forEach(element => {
+      element.classList.remove('active');
       if (element.dataset.color === mainColors) {
-        
-        element.classList.add("active");
+        element.classList.add('active');
       }
     });
-
 }
 
 let bckOpt = true;
-
 let bckIntv;
-
-let bckLclItm = localStorage.getItem("background_option");
-
+let bckLclItm = localStorage.getItem('background_option');
 if (bckLclItm !== null) {
-  
   if (bckLclItm === 'true') {
-
     bckOpt = true;
-
   } else {
-
     bckOpt = false;
-
   }
-  
-  document.querySelectorAll(".rnd-or-cstm-bck span").forEach(element => {
-
-    element.classList.remove("active");
-
+  document.querySelectorAll('.rnd-or-cstm-bck span').forEach(element => {
+    element.classList.remove('active');
   });
-
   if (bckLclItm === 'true') {
-
-    document.querySelector(".rnd-or-cstm-bck .yes").classList.add("active"); 
-
+    document.querySelector('.rnd-or-cstm-bck .yes').classList.add('active'); 
   } else {
-
-    document.querySelector(".rnd-or-cstm-bck .no").classList.add("active");
-
+    document.querySelector('.rnd-or-cstm-bck .no').classList.add('active');
   }
-
 }
 
 // Toggle Spin
-document.querySelector(".toggle-settings .fa-cog").onclick =  function () {
-
-  this.classList.toggle("fa-spin");
-
-  document.querySelector(".settings-box").classList.toggle("open");
-
+document.querySelector('.toggle-settings .fa-cog').onclick =  function () {
+  this.classList.toggle('fa-spin');
+  document.querySelector('.settings-box').classList.toggle('open');
 }
 
-const colorsLi = document.querySelectorAll(".colors-list li");
-
+const colorsLi = document.querySelectorAll('.colors-list li');
 colorsLi.forEach(li => {
-
-  li.addEventListener("click", (e) => {
-
+  li.addEventListener('click', (e) => {
     document.documentElement.style.setProperty('--main-color', e.target.dataset.color);
-
-    localStorage.setItem("color_option", e.target.dataset.color);
-
+    localStorage.setItem('color_option', e.target.dataset.color);
     hanActive(e);
-
   });
-
 });
 
-const rndBckOpt = document.querySelectorAll(".rnd-or-cstm-bck span");
-
+const rndBckOpt = document.querySelectorAll('.rnd-or-cstm-bck span');
 rndBckOpt.forEach(span => {
-
-  span.addEventListener("click", (e) => {
-
+  span.addEventListener('click', (e) => {
     hanActive(e);
-
     if (e.target.dataset.bck === 'yes') {
-
       bckOpt = true;
-
       rndImgs();
-
-      localStorage.setItem("background_option", true);
-
+      localStorage.setItem('background_option', true);
     } else {
-
       bckOpt = false;
-
       clearInterval(bckIntv);
-
-      localStorage.setItem("background_option", false);
-
+      localStorage.setItem('background_option', false);
     }
-
   });
-
 });
 
-let landPage = document.querySelector(".landing-page");
-
-let imgArray = ["ftfour.jpg", "fttwel.jpg", "ftfvtn.jpg", "fttwfv.jpg"];
+let landPage = document.querySelector('.landing-page');
+let imgArray = ['ftfour.jpg', 'fttwel.jpg', 'ftfvtn.jpg', 'fttwfv.jpg'];
 
 function rndImgs() {
-
   if(bckOpt === true) {
-    
     bckIntv = setInterval( () => {
-
       let randNum = Math.floor(Math.random() * imgArray.length);
-
-      landPage.style.backgroundImage = 'url("images/' + imgArray[randNum] + '")';
-
+      landPage.style.backgroundImage = `url('images/' + imgArray[randNum] + '')`;
       }, 3000 );
-  
     }
-
 }
 
 rndImgs();
 
-let ourGoods = document.querySelector(".products");
-
+let ourGoods = document.querySelector('.products');
 window.onscroll = function () {
-
   let skOfTop = ourGoods.offsetTop;
-
   let skOutHei = ourGoods.offsetHeight;
-
   let winHei = this.innerHeight;
-
   let winScrTop = this.pageYOffset;
-
   if (winScrTop > (skOfTop + skOutHei - winHei)) {
-
-
-    let allGoods = document.querySelectorAll(".product-box .product-progress span");
-
+    let allGoods = document.querySelectorAll('.product-box .product-progress span');
     allGoods.forEach(skill => {
-
       skill.style.width = skill.dataset.progress;
-
     });
-
   }
-
 };
 
-let ourGallery = document.querySelectorAll(".gallery img");
-
+let ourGallery = document.querySelectorAll('.gallery img');
 ourGallery.forEach(img => {
-
   img.addEventListener('click', (e) => {
-
-    let ovrl = document.createElement("div");
-
+    let ovrl = document.createElement('div');
     ovrl.className = 'ppp-overlay';
-
     document.body.appendChild(ovrl);
-
-    let pppBox = document.createElement("div");
-
+    let pppBox = document.createElement('div');
     pppBox.className = 'ppp-box';
-
     if (img.alt !== null) {
-
-      let imgHdg = document.createElement("h3");
-
+      let imgHdg = document.createElement('h3');
       let imgTxt = document.createTextNode(img.alt);
-
       imgHdg.appendChild(imgTxt);
-      
       pppBox.appendChild(imgHdg);
-
     }
-
-    let pppImage = document.createElement("img");
-
+    let pppImage = document.createElement('img');
     pppImage.src = img.src;
-
     pppBox.appendChild(pppImage);
-
     document.body.appendChild(pppBox);
-
     // Create the close span (The X sign) 
-    let closeButton = document.createElement("span");
-
-    let closeButtonTxt = document.createTextNode("X");
-
+    let closeButton = document.createElement('span');
+    let closeButtonTxt = document.createTextNode('X');
     closeButton.appendChild(closeButtonTxt);
-
     closeButton.className = 'close-button';
-
     pppBox.appendChild(closeButton);
-
   });
-
 });
 
-
-document.addEventListener("click", function (e) {
-
+document.addEventListener('click', function (e) {
   if (e.target.className == 'close-button') {
-
     e.target.parentNode.remove();
-
-    document.querySelector(".ppp-overlay").remove();
-
+    document.querySelector('.ppp-overlay').remove();
   }
-
 });
 
-
-const allBull = document.querySelectorAll(".nav-bullets .bullet");
-
-
-
-const allLinxx = document.querySelectorAll(".links a");
+const allBull = document.querySelectorAll('.nav-bullets .bullet');
+const allLinxx = document.querySelectorAll('.links a');
 
 function scrToTar(elements) {
-
   elements.forEach(element => {
-
-    element.addEventListener("click", (e) => {
-
+    element.addEventListener('click', (e) => {
       e.preventDefault();
-
       document.querySelector(e.target.dataset.section).scrollIntoView({
-
         behavior: 'smooth'
-
       });
-
     });
-
   });
-
 }
-
 scrToTar(allBull);
 scrToTar(allLinxx);
 
 //Handling active status 
 function hanActive(evt) {
-
-    evt.target.parentElement.querySelectorAll(".active").forEach(element => {
-
-      element.classList.remove("active");
-
+    evt.target.parentElement.querySelectorAll('.active').forEach(element => {
+      element.classList.remove('active');
     });
-
-    evt.target.classList.add("active");
-
+    evt.target.classList.add('active');
 }
 
 // Reset Button
-document.querySelector(".reset-option").onclick = function () {
-
-  localStorage.removeItem("color_option");
-
-  localStorage.removeItem("background_option");
-
+document.querySelector('.reset-option').onclick = function () {
+  localStorage.removeItem('color_option');
+  localStorage.removeItem('background_option');
   window.location.reload();
-
 };
 
-
-let toggleBtn = document.querySelector(".toggle-menu");
-let tLinks = document.querySelector(".links");
-
+let toggleBtn = document.querySelector('.toggle-menu');
+let tLinks = document.querySelector('.links');
 toggleBtn.onclick = function (e) {
-
   e.stopPropagation();
-
-  this.classList.toggle("menu-active");
-
-  tLinks.classList.toggle("open");
-
+  this.classList.toggle('menu-active');
+  tLinks.classList.toggle('open');
 };
 
 // Click anywhere to hide 
-document.addEventListener("click", (e) => {
-
+document.addEventListener('click', (e) => {
   if (e.target !== toggleBtn && e.target !== tLinks) {
-
-    if (tLinks.classList.contains("open")) {
-
-      toggleBtn.classList.toggle("menu-active");
-
-
-      tLinks.classList.toggle("open");
-
+    if (tLinks.classList.contains('open')) {
+      toggleBtn.classList.toggle('menu-active');
+      tLinks.classList.toggle('open');
     }
-
   }
-
 });
 
 // Stop Propagation on menu
 tLinks.onclick = function (e) {
-
   e.stopPropagation();
-
 }
